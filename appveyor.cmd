66214390a570b40694ae94b95795c52d0dc4a2a9
maniglia <kSociety85@gmail.com>
2021-11-09 13:48:44 +0100

    Visual Studio 2022.

--- appveyor.cmd
+++ appveyor.cmd
@@ -9,11 +9,11 @@ msbuild -p:Configuration=%_C% || exit /b
 
 msbuild Src\01\01\Web\KSociety.Com.Pre.Web.App\KSociety.Com.Pre.Web.App.csproj -t:restore -p:Configuration=%_C% || exit /b
 
-msbuild Src\01\01\Web\KSociety.Com.Pre.Web.App\KSociety.Com.Pre.Web.App.csproj -t:Publish -p:Configuration=%_C% -p:SelfContained=false -p:Platform="Any CPU" -p:TargetFramework="net5.0" -p:OutputPath=%_P%\KSociety.Com.Pre.Web.App\%_C%\net5.0\ || exit /b
+msbuild Src\01\01\Web\KSociety.Com.Pre.Web.App\KSociety.Com.Pre.Web.App.csproj -t:Publish -p:Configuration=%_C% -p:SelfContained=false -p:Platform="Any CPU" -p:TargetFramework="net6.0" -p:OutputPath=%_P%\KSociety.Com.Pre.Web.App\%_C%\net6.0\ || exit /b
 
 msbuild Src\01\02\Host\KSociety.Com.Srv.Host\KSociety.Com.Srv.Host.csproj -t:restore -p:Configuration=%_C% || exit /b
 
-msbuild Src\01\02\Host\KSociety.Com.Srv.Host\KSociety.Com.Srv.Host.csproj -t:Publish -p:Configuration=%_C% -p:SelfContained=false -p:Platform="Any CPU" -p:TargetFramework="net5.0" -p:OutputPath=%_P%\KSociety.Com.Srv.Host\%_C%\net5.0\ || exit /b
+msbuild Src\01\02\Host\KSociety.Com.Srv.Host\KSociety.Com.Srv.Host.csproj -t:Publish -p:Configuration=%_C% -p:SelfContained=false -p:Platform="Any CPU" -p:TargetFramework="net6.0" -p:OutputPath=%_P%\KSociety.Com.Srv.Host\%_C%\net6.0\ || exit /b
 
 msbuild Src\00\KSociety.Com.Install\KSociety.Com.Install.csproj -t:restore -p:Configuration=%_C% || exit /b
 
